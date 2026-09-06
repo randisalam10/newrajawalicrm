93b3680f952af1b8f30566e262a6b5e7f55f3403
randisalam10 <randisalam10@gmail.com>
2026-09-07 00:44:12 +0700

    chore(deploy): bump DEFAULT_IMAGE_TAG to v2.1.0

--- deploy.sh
+++ deploy.sh
@@ -21,7 +21,7 @@ NC='\033[0m'
 
 APP_NAME="rajawali-app"
 DEFAULT_IMAGE_NAME="randisalam1007/rajawali-bp-erp"
-DEFAULT_IMAGE_TAG="v2.0.9"
+DEFAULT_IMAGE_TAG="v2.1.0"
 
 echo -e "${BLUE}================================================${NC}"
 echo -e "${BLUE} 🚀 Memulai Deployment Otomatis Rajawali BP ERP  ${NC}"
